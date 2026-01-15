256ea14c6fbfda655f1e6af1c42cfcafa396e681
cw0527 <iscurt.w@gmail.com>
2026-01-14 16:44:27 -0800

    created chisquares.py, greated goon.md, deletd everything inside elo_model.py

---
+++ python/utils/chisquares.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.stats import chi2_contingency
+
+# Your observed data (counts)
+observed = np.array([
+    [10, 20],
+    [30, 40]
+])
+
+chi2, p, dof, expected = chi2_contingency(observed)
+
+print("Chi-square:", chi2)
+print("p-value:", p)
